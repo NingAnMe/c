69cb033c0345131e5f4f3ad6620f033c6a523753
NingAnMe <ninganme@qq.com>
2018-08-04 11:26:03 +0800

    第一个c程序

---
+++ first.c
@@ -0,0 +1,15 @@
+//
+// Created by NingAnMe on 2018/8/4.
+//
+
+#include <stdio.h>
+
+int main(void)  /* 一个简单的C程序 */
+{
+    int num;
+    num = 1;
+    printf("I am a simple ");
+    printf("computer. \n");
+    printf("My favorite number is %d because it is first.\n", num);
+    return 1;
+}
