e2b5326c984829ccc197090e08c1a84c43223acd
NingAnMe <ninganme@qq.com>
2018-08-04 12:03:29 +0800

    添加 fathm_ft.c 程序

---
+++ fathm_ft.c
@@ -0,0 +1,19 @@
+//
+// Created by NingAnMe on 2018/8/4.
+//
+
+// 把2音寻转换成英寸
+
+#include <stdio.h>
+
+int main(void)
+{
+    int feet, fathoms;
+    fathoms = 2;
+    feet = 6 * fathoms;
+
+    printf("There are %d feet in %d fathoms!\n", feet, fathoms);
+    printf("Yes, I said %d feet!\n", 6 * fathoms);
+
+    return 0;
+}
